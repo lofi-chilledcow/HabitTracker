2a3b1e5c07c9b146c5cd3dd31f5b433076ac3a98
lofi-chilledcow <nick.fan.sde@gmail.com>
2026-03-27 19:22:04 -0700

    fix: fix the CORS issue.

--- src/services/ApiGateway/Program.cs
+++ src/services/ApiGateway/Program.cs
@@ -4,11 +4,29 @@ Env.TraversePath().Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:3002",
+                "http://localhost:3003",
+                "http://localhost:3004"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
+app.UseCors();
 app.MapReverseProxy();
 
 app.Run();
